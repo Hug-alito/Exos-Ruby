d254c6d6e5608babb647d99e6a9e1b069de6e5ba
Hugo Rischmann <hugo.rischmann@me.com>
2023-04-12 09:08:52 +0200

    Modification annee oubliée

--- exo_9.rb
+++ exo_9.rb
@@ -1,7 +1,7 @@
 puts "Quelle est ta date de naissance ?"
 annee = gets.chomp.to_i
 puts "Ok c'est parti pour voir le nombre d'années parcourues !"
-(2023 - i + 1).times do |index|
+(2023 - annee + 1).times do |index|
   puts "#{annee}"
   annee = annee + 1
 end
